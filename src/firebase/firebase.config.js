4b4bb35a379590bf325556da81c955fd10480ee2
unknown <biplob2358@gmail.com>
2022-10-24 23:37:50 +0600

    fire base install and configer

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrDceoCL43G2Br9YXcGGGGxLv84OLT7KM",
+  authDomain: "course-guru-94acd.firebaseapp.com",
+  projectId: "course-guru-94acd",
+  storageBucket: "course-guru-94acd.appspot.com",
+  messagingSenderId: "653093595180",
+  appId: "1:653093595180:web:ed0fd764c977dfe9860989",
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
